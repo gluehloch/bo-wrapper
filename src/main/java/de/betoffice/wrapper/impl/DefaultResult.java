8642c4e3f14501196451655eebdb1961eff3fc54
Andre Winkler <gluehloch@googlemail.com>
2022-09-25 16:20:44 +0200

    Generic types ...

--- src/main/java/de/betoffice/wrapper/impl/DefaultResult.java
+++ src/main/java/de/betoffice/wrapper/impl/DefaultResult.java
@@ -1,3 +1,26 @@
+/*
+ * ============================================================================
+ * Project betoffice-wrapper Copyright (c) 2000-2022 by Andre Winkler. All
+ * rights reserved.
+ * ============================================================================
+ * GNU GENERAL PUBLIC LICENSE TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND
+ * MODIFICATION
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 package de.betoffice.wrapper.impl;
 
 import java.util.Objects;
@@ -16,12 +39,12 @@ class DefaultResult<T> implements Result<T> {
 
     static <T> Result<T> success(T result) {
         Objects.nonNull(result);
-        return new DefaultResult(result, null);
+        return new DefaultResult<T>(result, null);
     }
 
     static <T> Result<T> failure(Throwable exception) {
         Objects.nonNull(exception);
-        return new DefaultResult(null, exception);
+        return new DefaultResult<T>(null, exception);
     }
 
     @Override
--- src/main/java/de/betoffice/wrapper/impl/TryGetCatcher.java
+++ src/main/java/de/betoffice/wrapper/impl/TryGetCatcher.java
@@ -1,3 +1,26 @@
+/*
+ * ============================================================================
+ * Project betoffice-wrapper Copyright (c) 2000-2022 by Andre Winkler. All
+ * rights reserved.
+ * ============================================================================
+ * GNU GENERAL PUBLIC LICENSE TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND
+ * MODIFICATION
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 package de.betoffice.wrapper.impl;
 
 import java.util.function.Supplier;
@@ -6,7 +29,7 @@ import de.betoffice.wrapper.api.Result;
 
 public class TryGetCatcher {
 
-     static <T> Result tryGetCatch(Supplier<T> supplier) {
+     static <T> Result<T> tryGetCatch(Supplier<T> supplier) {
         try {
             return DefaultResult.success(supplier.get());
         } catch (Throwable ex) {
